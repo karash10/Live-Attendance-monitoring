5a0a245e8ec74a7467a2b331271ac3ec4637d302
karash10 <kappalaharshith@gmail.com>
2026-01-26 18:57:29 +0530

    feat: initialised the repo and folder structure

---
+++ src/main/java/com/attendance/attendancebackend/AttendancebackendApplication.java
@@ -0,0 +1,13 @@
+package com.attendance.attendancebackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendancebackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendancebackendApplication.class, args);
+	}
+
+}
